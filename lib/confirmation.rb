50a9295580c49fb2442b05afd63fe00a8283cb9e
Sean Peters <sean@snptrs.dev>
2023-05-19 11:34:11 +0100

    Adds order confirmation and unit tests

--- lib/confirmation.rb
+++ lib/confirmation.rb
@@ -1,5 +1,22 @@
+require 'twilio-ruby'
+
 class Confirmation
+  def initialize
+    @twilio_sid = ENV["TWILIO_SID"]
+    @twilio_token = ENV["TWILIO_AUTH_TOKEN"]
+    @twilio_number = ENV["TWILIO_PHONE_NUMBER"]
+    @my_number = ENV["MY_PHONE_NUMBER"]
+  end
+  
   def send(order, phone_number)
-    
+    fail "No phone number provided" if phone_number.empty?
+
+    @client = Twilio::REST::Client.new @twilio_sid, @twilio_token
+    message = @client.messages.create(
+      body: "Order #{order.order_id} is on its way!",
+      to: phone_number,  # Text this number
+      from: @twilio_number, # From a valid Twilio number
+    )
+    return message.sid
   end
 end
--- spec/confirmation_spec.rb
+++ spec/confirmation_spec.rb
@@ -1,11 +1,16 @@
 require 'confirmation'
 
-RSpec.describe do
+RSpec.describe "Confirmation unit test" do
   context "when sending an order confirmation" do
     let(:order) { double(:order, order_id: "1234") }
     it "sends an order confirmation" do
       confirmation = Confirmation.new
-      confirmation.send(order, "07700000000")
+      expect(confirmation.send(order, ENV["MY_PHONE_NUMBER"])).to be_a(String)
+    end
+    
+    it "fails if no phone number provided" do
+      confirmation = Confirmation.new
+      expect { confirmation.send(order, "") }.to raise_error "No phone number provided"
     end
   end
 end
