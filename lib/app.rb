e0260dc9fb07345df18d1488d683f46afaf797ce
Sean Peters <sean@snptrs.dev>
2023-05-22 12:44:19 +0100

    Changed output handling in App

--- lib/app.rb
+++ lib/app.rb
@@ -2,19 +2,22 @@ require_relative 'menu'
 require_relative 'order'
 require_relative 'receipt'
 require_relative 'confirmation'
+require_relative 'output'
 
 class App
   def initialize
     @menu = Menu.new
-    @order = ""
+    @output = Output.new
   end
+  
   def user_input
     loop do
-      puts "What would you like to do?"
-      puts "1. See the menu"
-      puts "2. Place an order"
-      puts "3. View receipt"
-      puts "4. Send order confirmation"
+      @output.string("What would you like to do?")
+      @output.string("1. See the menu")
+      @output.string("2. Place an order")
+      @output.string("3. View receipt")
+      @output.string("4. Send order confirmation")
+      @output.string("5. Exit")
       input = gets.chomp
       break if input == ""
       controller(input)
@@ -31,6 +34,8 @@ class App
       view_receipt
     when "4"
       send_confirmation  
+    when "5"
+      exit
     end
   end
   
@@ -38,30 +43,32 @@ class App
     @order = Order.new
     loop do
       @menu.show
-      puts "Choose an item from the menu. Just press enter when you're done."
+      @output.string("Choose an item from the menu. Just press enter when you're done.")
       selection = gets.chomp
       break if selection == ""
-      @order.add(@menu.choose(selection.to_i - 1))
+      @order.add(@menu.choose(selection.to_i))
+      @output.string("#{@menu.choose(selection.to_i).description} added to order", :green, :before)
     end
-    puts "🎉 Order ##{@order.order_id} placed!\n\n"
+    @output.string("🎉 Order ##{@order.order_id} placed!", :blue, :after)
   end
   
   def view_receipt
+    return @output.error("No order placed yet", :pad) if @order == nil
     receipt = Receipt.new(@order)
-    puts "\n===RECEIPT FOR ORDER ##{@order.order_id}===\n"
+    @output.header("RECEIPT FOR ORDER ##{@order.order_id}")
     receipt.itemise.each do |item|
-      puts item
+      @output.string(item)
     end
-    puts "GRAND TOTAL: #{sprintf("£%.2f", receipt.total)}"
-    puts "\n"
+    @output.string("GRAND TOTAL: #{@output.currency(receipt.total)}", :blue, :after)
   end
 
   def send_confirmation
-    puts "Enter your phone number:"
+    @output.string("Enter your phone number:")
     phone_number = gets.chomp
+    return @output.error("You need to enter a phone number", :after) if phone_number == ""
     confirmation = Confirmation.new
     confirmation.send(@order, phone_number)
-    puts "\n✅ Order confirmation sent.\n\n"
+    @output.string("✅ Order confirmation sent.", :light_green, :pad)
   end
 end
 
